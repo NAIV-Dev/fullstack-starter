10fa04bf0f14ac8bb3d21f1b64dd45d50ed29537
Rio <rio@graf.co.id>
2025-10-27 16:16:09 +0700

    update head title

--- web/src/routes/__root.tsx
+++ web/src/routes/__root.tsx
@@ -1,12 +1,21 @@
-import { Outlet, createRootRoute } from '@tanstack/react-router'
+import { HeadContent, Outlet, createRootRoute } from '@tanstack/react-router'
 import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
 import { TanStackDevtools } from '@tanstack/react-devtools'
+import { useSystemConfig } from '@/hooks/useSystemConfig'
 
 export const Route = createRootRoute({
-  component: () => (
-    <>
-      <Outlet />
-      {/* <TanStackDevtools
+  component: () => {
+    const { system_name } = useSystemConfig();
+    return (
+      <>
+        <head>
+          <title>
+            { system_name }
+          </title>
+        </head>
+        <HeadContent />
+        <Outlet />
+        {/* <TanStackDevtools
         config={{
           position: 'bottom-right',
         }}
@@ -17,6 +26,7 @@ export const Route = createRootRoute({
           },
         ]}
       /> */}
-    </>
-  ),
+      </>
+    )
+  },
 })
